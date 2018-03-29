6d8a3560198acd2fcfa0e9895833787cffe297d8
Cristi Mitran <cristimitran@gmail.com>
2018-03-29 10:02:31 +0300

    added projects

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page',
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
